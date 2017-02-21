93c57944ed27eec4dcf012e6b41795e736bce624
neverchanje <wutao163.com@163.com>
2017-02-21 20:16:00 +0800

    Fix installation error.
    Git-checkout the dependencies to the version being used.
    Print a message to tell a dependency is being installed.
    Use set -e to abort building when some execution is failed.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+
+# Exit if error happens.
+set -e
+
 DO_TEST=0
 
 while getopts "t" arg
@@ -22,15 +26,18 @@ PROTO_FILES_DIR=`pwd`/pb
 mkdir -p ${FLAG_PREFIX}
 
 # glog
+echo "Installing glog"
 if [ ! -f "${FLAG_PREFIX}/glog_0_3_4" ] \
 	|| [ ! -d "${DEPS_DIR}/glog" ]; then
 	cd ${DEPS_DIR}/glog
+    git checkout v0.3.4
 	./configure --prefix=${DEPS_PREFIX} --disable-shared
 	make -j4 && make install
 	touch "${FLAG_PREFIX}/glog_0_3_4"
 fi
 
 # silly
+echo "Installing silly"
 if [ ! -f "${FLAG_PREFIX}/silly" ] \
 	|| [ ! -d "${DEPS_DIR}/silly" ]; then
 	cd ${DEPS_DIR}/silly
@@ -41,11 +48,12 @@ if [ ! -f "${FLAG_PREFIX}/silly" ] \
 fi
 
 # protobuf
+echo "Installing protobuf"
 if [ ! -f "${FLAG_PREFIX}/protobuf_2_7_0" ] \
 	|| [ ! -d "${DEPS_DIR}/protobuf" ]; then
 	cd ${DEPS_DIR}/protobuf
+    sudo apt-get -y install autoconf automake libtool curl make g++ unzip
     git checkout origin/2.7.0
-    ./autogen.sh
     ./configure --prefix=${DEPS_PREFIX} --disable-shared
 	make -j4 && make install
 	touch "${FLAG_PREFIX}/protobuf_2_7_0"
@@ -53,15 +61,19 @@ fi
 
 # googletest
 if [ ${DO_TEST} -eq 1 ]; then
+    echo "Installing googletest"
     if [ ! -f "${FLAG_PREFIX}/googletest_1_8_0" ] \
         || [ ! -d "${DEPS_DIR}/googletest" ]; then
         cd ${DEPS_DIR}/googletest
+        git checkout release-1.8.0
         mkdir -p build && cd build
         cmake .. -DCMAKE_INSTALL_PREFIX=${DEPS_PREFIX}
         make -j4 && make install
         touch "${FLAG_PREFIX}/googletest_1_8_0"
     fi
 fi
+
+echo "Installing yaraft"
 
 cd ${BUILD_DIR}
 if [ ${DO_TEST} -eq 1 ]; then
